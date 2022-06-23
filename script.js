245d0072d30b6ad0df1a3984956cdd74d4b70c75
lilys tacos <lilyannek@gmail.com>
2022-06-23 11:42:21 -0700

    initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+var wins = 0;
+var losses = 0;
+var ties = 0;
+
+let playGame = prompt("Rock, paper, or scissors?");
+switch(playGame) {
+    case "Rock":
+    text = "You've chosen rock.";
+    break;
+
+    case "Scissors":
+    text = "You've chosen scissors.";
+    break;
+
+    case "Paper":
+}
